fcbf56b7e4519b0c5533e6c1e78b400a4647bc3e
User <patricia_franco4320@hotmail.com>
2023-10-17 14:00:58 -0300

    Bootcamp Santander Dio

---
+++ src/test/java/dio/santanderbootcampdio/SantanderBootcampDioApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.santanderbootcampdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SantanderBootcampDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
